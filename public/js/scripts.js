4ca166973b369133293721d869430ead0c946061
jiljiang <jiljiang@ebay.com>
2016-05-16 22:55:44 +0800

    update blog

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -97,7 +97,7 @@
 (function() {
 	
 
-	var app = angular.module('app', ['ngRoute', 'configCtrl', 'blogCtrl', 'ui.bootstrap']);
+	var app = angular.module('app', ['ngRoute', 'configCtrl', 'blogCtrl', 'memoryCtrl', 'ui.bootstrap']);
 
 	app.config(function ($routeProvider) {
 		// ==========================================
@@ -115,6 +115,13 @@
 		}).when('/blog/:createTime', {
 			templateUrl: 'partials/blog/blog.html',
 			controller: 'blogCtrl'
+
+		// ==========================================
+		// =                 Memory                 =
+		// ==========================================
+		}).when('/memory', {
+			templateUrl: 'partials/memory/list.html',
+			controller: 'memoryList'
 
 		// ==========================================
 		// =                 Config                 =
@@ -610,6 +617,85 @@
 (function() {
 	
 
+	angular.module('app').factory("Memory", function (File, Asset, $http, $q) {
+		var _list;
+		var _path_list = "data/memoryList.json";
+
+		var Memory = function() {};
+
+		Memory.ready = false;
+
+		// Blog list
+		Memory.list = function(forceRefresh) {
+			if(!_list || forceRefresh) {
+				_list = _list || {};
+				$http.get("data/memoryList.json?_=" + (+new Date())).then(function(data) {
+					_list = $.extend(_list, data.data);
+				}).finally(function() {
+					Memory.ready = true;
+				});
+			}
+			return _list;
+		};
+
+		// Save blog
+		Memory.save = function(memory) {
+			var _deferred = $q.defer();
+
+			// Write blog
+			var data;
+
+			// Read blog list
+			File.read(File.path(_path_list)).then(function(_data) {
+				data = _data;
+			}).finally(function() {
+				var _list;
+				try {
+					_list = JSON.parse(data);
+				} catch (err) {
+					_list = {};
+				}
+				_list.memories = _list.memories || [];
+				_list.memories.push(memory);
+
+				// Update list
+				File.write(File.path(_path_list), JSON.stringify(_list, null, "\t")).then(function() {
+					_deferred.resolve();
+					Memory.list(true);
+				}, function(err) {
+					_deferred.reject(err);
+				});
+			}, function(err) {
+				_deferred.reject(err);
+			});
+
+			return _deferred.promise;
+		};
+
+		Memory.delete = function (mem) {
+			var _deferred = $q.defer();
+			common.array.remove(mem, _list.memories);
+
+			// Update list
+			File.write(File.path(_path_list), JSON.stringify(_list, null, "\t")).then(function() {
+				_deferred.resolve();
+				Memory.list(true);
+				if(mem.thumbnail) {
+					Asset.delete(mem.thumbnail.replace("data/assets/", ""));
+				}
+			}, function(err) {
+				_deferred.reject(err);
+			});
+
+			return _deferred.promise;
+		};
+
+		return Memory;
+	});
+})();
+(function() {
+	
+
 	angular.module('app').factory("Page", function () {
 		var _origin = {
 			title: "",
@@ -907,5 +993,95 @@
 				});
 			});
 		};
+	});
+})();
+(function() {
+	
+
+	var memoryCtrl = angular.module('memoryCtrl', ['ngRoute', 'ui.bootstrap']);
+
+	// ==============================================================
+	// =                            List                            =
+	// ==============================================================
+	memoryCtrl.controller('memoryList', function ($scope, Page, Memory, Asset) {
+		Page.title = "Memory";
+
+		$scope.new = {};
+		var _tmpImageFile = "";
+
+		var $memTip = $("#memTip");
+		$memTip.hide();
+
+		// Memory list
+		$scope.memoryHolder = Memory.list();
+
+		// Memory tooltip
+		$scope.enterMemory = function (mem, $event) {
+			var $tgt = $($event.currentTarget);
+			$scope.currentMemory = mem;
+			$memTip.stop().fadeIn();
+
+			// Position
+			var winWidth = $(window).width();
+			var tgtOffset = $tgt.offset();
+			var offset = $.extend({}, tgtOffset);
+			offset.left += $tgt.outerWidth();
+			offset.top -= 35;
+			if(offset.left + $memTip.outerWidth() > winWidth) {
+				offset.left = tgtOffset.left - $memTip.outerWidth();
+			}
+			$memTip.offset(offset);
+		};
+		$scope.leaveMemory = function () {
+			$memTip.stop().fadeOut();
+		};
+
+		// Memory update
+		$scope.newMemory = function () {
+			$scope.new = {};
+			_tmpImageFile = "";
+			$("#memoryMDL").modal();
+		};
+		$scope.saveMemory = function () {
+			_tmpImageFile = "";
+			$("#memoryMDL").modal("hide");
+
+			$scope.createTime = +new Date();
+			Memory.save($scope.new);
+		};
+		$scope.deleteMemory = function (mem) {
+			if(Page.local) {
+				$.dialog({
+					title: "Delete Confirm",
+					content: "Do you want to delete '" + mem.title + "'?",
+					confirm: true
+				}, function (ret) {
+					if(ret) {
+						Memory.delete(mem);
+					}
+				});
+			}
+		};
+
+		$("#memoryMDL").on("dragover", ".memoryWindow", function (event) {
+			event.preventDefault();
+		});
+		$("#memoryMDL").on("drop", ".memoryWindow", function(event) {
+			var files = event.originalEvent.dataTransfer.files;
+			if(files.length) {
+				event.preventDefault();
+				event.stopPropagation();
+
+				Asset.upload([files[0]]).then(function(list) {
+					_tmpImageFile = list[0].name;
+					$scope.new.thumbnail = "data/assets/" + _tmpImageFile;
+				});
+			}
+		});
+		$("#memoryMDL").on("hidden.bs.modal", function () {
+			if(_tmpImageFile) {
+				Asset.delete(_tmpImageFile);
+			}
+		});
 	});
 })();
