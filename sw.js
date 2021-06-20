d6022ee6f54be00e6dd0a388064b373fec30691d
zombiej <smith3816@gmail.com>
2021-06-20 11:57:05 +0800

    chore: Update sw.js cache

--- sw.js
+++ sw.js
@@ -1,64 +1,56 @@
 const CACHE_NAME = 'aizhuzi';
 
-const PRECACHE_LIST = [
-  './dist/static/umi.js',
-  './dist/static/umi.css',
-  './dist/static/common-umi.async.js',
-  './dist/static/src__layouts__index.async.js',
-  './dist/static/src__pages__blog__$id__index.async.js',
-  './dist/static/src__pages__blog__index.async.js',
-  './dist/static/src__pages__index.async.js',
-  './dist/static/src__pages__memory__index.async.js',
-];
+const PRECACHE_LIST = ['./dist/umi.js', './dist/umi.css'];
 
-self.addEventListener('install', e => {
+self.addEventListener('install', (e) => {
   e.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(PRECACHE_LIST)
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache
+        .addAll(PRECACHE_LIST)
         .then(self.skipWaiting())
-        .catch(err => console.log(err))
-    })
-  )
+        .catch((err) => console.log(err));
+    }),
+  );
 });
 
-self.addEventListener('activate', event => {
+self.addEventListener('activate', (event) => {
   // delete old deprecated caches.
-  caches.keys().then(cacheNames => Promise.all(
-    cacheNames
-      .filter(cacheName => cacheName !== CACHE_NAME)
-      .map(cacheName => caches.delete(cacheName))
-  ))
-  console.log('service worker activated.')
+  caches
+    .keys()
+    .then((cacheNames) =>
+      Promise.all(
+        cacheNames
+          .filter((cacheName) => cacheName !== CACHE_NAME)
+          .map((cacheName) => caches.delete(cacheName)),
+      ),
+    );
+  console.log('service worker activated.');
   event.waitUntil(self.clients.claim());
 });
-
 
 // const isNavigationReq = (req) => (req.mode === 'navigate' || (req.method === 'GET' && req.headers.get('accept').includes('text/html')))
 // const endWithExtension = (req) => Boolean(new URL(req.url).pathname.match(/\.\w+$/))
 // const shouldRedirect = (req) => (isNavigationReq(req) && new URL(req.url).pathname.substr(-1) !== "/" && !endWithExtension(req))
 
-self.addEventListener('fetch', event => {
+self.addEventListener('fetch', (event) => {
   if (event.request.method.toUpperCase() !== 'GET') {
     return;
   }
 
   // const fetchPromise = fetch(event.request, { cache: "no-store" });
   const fetchPromise = fetch(event.request);
-  const fetchPromiseClone = fetchPromise.then(res => res.clone());
+  const fetchPromiseClone = fetchPromise.then((res) => res.clone());
 
   // 缓存
   fetchPromiseClone.then((res) => {
-    if(!res || res.status !== 200 || res.type !== 'basic') {
+    if (!res || res.status !== 200 || res.type !== 'basic') {
       return;
     }
 
-    caches.open(CACHE_NAME)
-      .then((cache) => {
-        cache.put(event.request, res.clone());
-      });
+    caches.open(CACHE_NAME).then((cache) => {
+      cache.put(event.request, res.clone());
+    });
   });
 
-  event.respondWith(
-    fetchPromise.catch(err => caches.match(event.request))
-  );
+  event.respondWith(fetchPromise.catch((err) => caches.match(event.request)));
 });
